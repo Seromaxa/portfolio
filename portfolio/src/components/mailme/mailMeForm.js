9431e450cf2a5301780cdd7101656b44a84685e6
Seromaxa <sergromanenko@gmail.com>
2020-10-21 16:22:54 +0300

    some test

--- portfolio/src/components/mailme/mailMeForm.js
+++ portfolio/src/components/mailme/mailMeForm.js
@@ -1,98 +1,99 @@
-import React,{useEffect,useRef} from 'react'
-import {init,send} from 'emailjs-com'
-import {filter} from '../../functions/functions'
-import Input from  '../../UI/input/Input'
+import React, { useEffect, useRef } from 'react'
+import { init, send } from 'emailjs-com'
+import { filter } from '../../functions/functions'
+import Input from '../../UI/input/Input'
 import Button from '../../UI/button/Button'
-import {connect} from 'react-redux'
-import {closeMail} from '../../store/actions/modalsAction'
+import { connect } from 'react-redux'
+import { closeMail } from '../../store/actions/modalsAction'
 
 
 
-const MailForm = props =>{
+const MailForm = props => {
 
 
-const name = useRef()
-const mail = useRef()
-const text = useRef()
+    const name = useRef()
+    const mail = useRef()
+    const text = useRef()
 
     const active = filter(props.lang.languages).active
-   
-    const submitHandler = ev =>{
+
+    const submitHandler = ev => {
         ev.preventDefault()
     }
-    const clicker = event=>{
-        if(event.code === 'Escape'){
-     return props.closeHandler()
+    const clicker = event => {
+        if (event.code === 'Escape') {
+            return props.closeHandler()
         }
-        
+
     }
-   
-   useEffect(()=>{
-       window.addEventListener('keydown',clicker)
-       return()=>{
-           window.removeEventListener('keydown',clicker)
-       }
-   })
-useEffect(()=>{
-   
-},[])
 
-const sendHandler = ()=>{
-  
-  name.current.value = ''
-  mail.current.value = ''
-  text.current.value = ''
-  props.closeHandler()
+    useEffect(() => {
+        window.addEventListener('keydown', clicker)
+        return () => {
+            window.removeEventListener('keydown', clicker)
+        }
+    })
+    useEffect(() => {
+
+    }, [])
+
+    const sendHandler = () => {
+        init("user_zYLWMAzxO9riuM0vp56Ec")
+        send('service_8hj8g78', 'template_wdl3hhi', { name: name.current.value, mail: mail.current.value, message: text.current.value })
+        name.current.value = ''
+        mail.current.value = ''
+        text.current.value = ''
+        props.closeHandler()
+    }
+
+    return (
+        <div className='form_wrapper'>
+
+            <form className='form' onSubmit={ev => submitHandler(ev)}>
+                <h2>{active.contactMe}</h2>
+                <div className='closer form_closer_position' onClick={props.closeHandler} >&#10008;</div>
+                <Input
+                    labelHero={active.yourName}
+                    inputType='text'
+                    name='name'
+                    refs={name}
+                />
+                <Input
+                    labelHero={active.mail}
+                    inputType='email'
+                    name='senderMail'
+                    refs={mail}
+                />
+                <div>
+                    <label htmlFor='messageText'>{active.message}</label>
+                    <textarea id='messageText' name='message' ref={text} />
+                </div>
+                <Button
+                    buttonHero={active.send}
+                    onClick={sendHandler}
+                />
+            </form>
+        </div>
+    )
+
 }
 
-return(
-    <div className='form_wrapper'>
-         
-    <form className='form' onSubmit={ev=>submitHandler(ev)}>
-<h2>{active.contactMe}</h2>
-    <div className='closer form_closer_position' onClick ={props.closeHandler} >&#10008;</div>
-<Input 
-labelHero={active.yourName}
-inputType='text'
-name='name'
-refs = {name}
-/>
-<Input 
-labelHero={active.mail}
-inputType='email'
-name='senderMail'
-refs = {mail}
- />
-<div>
-<label htmlFor='messageText'>{active.message}</label>
-    <textarea id='messageText' name='message' ref={text}/>
-</div>
-<Button
- buttonHero={active.send}
- onClick = {sendHandler}
- />
-    </form>
-    </div>
-)
-
-}
-
-function mapStateToProps(state){
-    return{
+function mapStateToProps(state) {
+    return {
         openMail: state.modalReducer.mail,
         lang: state.langReducer
 
     }
 
 }
-function mapDispatchToProps(dispatch){
-    return{
-        closeHandler: ()=>dispatch(closeMail())
+function mapDispatchToProps(dispatch) {
+    return {
+        closeHandler: () => dispatch(closeMail())
     }
-    
+
 }
 
 
 
 
-export default connect (mapStateToProps,mapDispatchToProps)(MailForm)
+export default connect(mapStateToProps, mapDispatchToProps)(MailForm)
